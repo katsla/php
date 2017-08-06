1c8c4195d943b8943271b5161afffe07e178c543
katsla <katsla@yandex.ru>
2017-08-07 00:36:54 +1200

    add functions

--- user_upload.php
+++ user_upload.php
@@ -15,56 +15,24 @@ if (!file_exists($csvfile)) {
     die("CSV file not found.\n");
 }
 
-ini_set('auto_detect_line_endings', true);
 
-$handle = fopen($csvfile, 'r');
-
-if (!$handle) {
-
-    die("Could not open the data file.\n");
-}
 
 // try to connect to DB
 
-try {
+// try {
 
-    $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (PDOException $e) {
+//    $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+//}
+//catch (PDOException $e) {
 
-    die("DB connection failed: ".$e->getMessage());
-}
+//    die("DB connection failed: ".$e->getMessage());
+//}
 
-echo "\nConnected to server.\n\n";
+// echo "\nConnected to server.\n\n";
 
-fgetcsv($handle); // ignore the first line
+// $conn = null;
 
-while (($data = fgetcsv($handle)) !== FALSE ) {
-
-    $charlist = " \t\n\r\0..\x40\x5B..\x60\x7B..\x7F";
-
-    $name = ucwords(strtolower(trim($data[0], $charlist)));
-
-    $surname = ucwords(strtolower(trim($data[1], $charlist)));
-    $surname = preg_replace_callback("/^O\'([a-z])/", function($match) { return strtoupper("$match[0]"); }, $surname); //processing O'Hara etc.
-
-    $email = strtolower($data[2]);
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-        echo "Email $email is not valid. Row will not be added to DB.\n";
-        continue;
-    }
-
-    echo $name.", ".$surname.", ".$email,"\n";
-
-    insert_value($name, $surname, $email);
-}
-
-$conn = null;
-
-fclose($handle);
+process_csv($csvfile);
 
 function insert_value($name, $surname, $email) {
 
@@ -77,9 +45,47 @@ function insert_value($name, $surname, $email) {
     $ins->execute();
 }
 
+function process_csv($csvfile, $db_connect = false) {
 
-// $loaded = $con -> exec("LOAD DATA LOCAL INFILE ".$con->quote($csvfile)." INTO TABLE `$dbtable` FIELDS TERMINATED BY ".$con->quote($fieldseparator)." LINES TERMINATED BY ".$con->quote($lineseparator)." IGNORE 1 LINES (name, surname, email)");
+ini_set('auto_detect_line_endings', true);
 
-// echo "Loaded a total of $loaded records. \n";
+$handle = fopen($csvfile, 'r') or die("Could not open the data file.\n");
+
+fgetcsv($handle) or die("Incorrect CSV file!\n");
+
+while (($data = fgetcsv($handle)) !== FALSE ) {
+
+    $charlist = " \t\n\r\0..\x40\x5B..\x60\x7B..\x7F"; // list of invalid characters
+
+    $data[0] = trim($data[0], $charlist);
+    $data[1] = trim($data[1], $charlist);
+
+    $data = array_map('strtolower', $data);
+
+    $name = ucwords($data[0]);
+    $surname = ucwords($data[1]);
+
+    $surname = preg_replace_callback("/^O\'([a-z])/", function($match) { return strtoupper("$match[0]"); }, $surname);
+
+    $email = filter_var($data[2], FILTER_SANITIZE_EMAIL);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        echo "\nInvalid row: incorrect email $email.\n\n";
+        continue;
+    }
+
+    if ($db_connect) {
+    
+        insert_value($name, $surname, $email);
+    } else {
+
+        echo "Valid row: $name, $surname, $email.\n";
+    }
+}
+
+fclose($handle);
+
+}
 
 ?>
