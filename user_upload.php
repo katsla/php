7603286b0c4784b97a1780544870730a82efbafe
katsla <katsla@yandex.ru>
2017-08-06 22:30:29 +1200

    move insert to function

--- user_upload.php
+++ user_upload.php
@@ -12,8 +12,7 @@ $csvfile = "users.csv";
 
 if (!file_exists($csvfile)) {
 
-	die("CSV file not found.\n");
-
+    die("CSV file not found.\n");
 }
 
 ini_set('auto_detect_line_endings', true);
@@ -22,70 +21,66 @@ $handle = fopen($csvfile, 'r');
 
 if (!$handle) {
 
-	die("Could not open the data file.\n");
-
+    die("Could not open the data file.\n");
 }
 
 // try to connect to DB
 
 try {
 
-	$con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::MYSQL_ATTR_LOCAL_INFILE => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-     
+    $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::MYSQL_ATTR_LOCAL_INFILE => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (PDOException $e) {
 
- }
- catch (PDOException $e) {
+    die("DB connection failed: ".$e->getMessage());
+}
 
-         die("DB connection failed: ".$e->getMessage());
-
- }
-
- echo "\nServer has been connected. \n\n";
+echo "\nConnected to server.\n\n";
 
 // to remove the first line
 fgetcsv($handle);
 
 while (($data = fgetcsv($handle)) !== FALSE ) {
-		
 
-        $charlist = " \t\n\r\0..\x40\x5B..\x60\x7B..\x7F";
+    $charlist = " \t\n\r\0..\x40\x5B..\x60\x7B..\x7F";
 
+    $name = ucwords(strtolower(trim($data[0], $charlist)));
+    $surname = ucwords(strtolower(trim($data[1], $charlist)));
 
-	$name = ucwords(strtolower(trim($data[0], $charlist)));
+    $surname = preg_replace_callback("/^O\'([a-z])/", function($match) { return strtoupper("$match[0]"); }, $surname);
+    $surname = str_replace("'", "\'", $surname);
 
-	$surname = ucwords(strtolower(trim($data[1], $charlist)));
-        
-        $surname = preg_replace_callback("/^O\'([a-z])/", function($match) { return strtoupper("$match[0]"); }, $surname);
+    $email = strtolower($data[2]);
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-        $surname = str_replace("'", "\'", $surname);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-	$email = strtolower($data[2]);
+        echo "Email $email is not valid. Row will not be added to DB.\n";
+        continue;
+    }
 
-	$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = str_replace("'", "\'", $email);
 
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo $name.", ".$surname.", ".$email,"\n";
 
-	 	echo "Email $email is not valid. Row will not be added to DB.\n";
-		continue;
-
-	}
-
-	$email = str_replace("'", "\'", $email);
-
-        echo $name.", ".$surname.", ".$email,"\n";
-
-        
-
-        $con -> exec("INSERT INTO $dbtable (name, surname, email) VALUES ('$name', '$surname', '$email');");
-	
-	
-
-
+    insert_value($name, $surname, $email);
 }
 
 $conn = null;
 
 fclose($handle);
+
+function insert_value($name, $surname, $email) {
+
+    global $con, $dbtable;
+
+    $ins = $con->prepare("INSERT INTO $dbtable (name, surname, email) VALUES (:name, :surname, :email);");
+    $ins->bindValue(':name', $name);
+    $ins->bindValue(':surname', $surname);
+    $ins->bindValue(':email', $email);
+    $ins->execute();
+
+}
 
 
 // $loaded = $con -> exec("LOAD DATA LOCAL INFILE ".$con->quote($csvfile)." INTO TABLE `$dbtable` FIELDS TERMINATED BY ".$con->quote($fieldseparator)." LINES TERMINATED BY ".$con->quote($lineseparator)." IGNORE 1 LINES (name, surname, email)");
