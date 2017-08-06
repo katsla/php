bae595598548d63f28922d1f96579a2a6b28d71d
katsla <katsla@yandex.ru>
2017-08-06 19:58:58 +1200

    string modification

--- user_upload.php
+++ user_upload.php
@@ -9,30 +9,65 @@ $fieldseparator = ",";
 $lineseparator = "\n";
 $csvfile = "users.csv";
 
-// echo "start";
 
 if (!file_exists($csvfile)) {
 
-die("File not found.\n");
+	die("CSV file not found.\n");
 
 }
 
-try {
-// ini_set('auto_detect_line_endings',TRUE);
+ini_set('auto_detect_line_endings', true);
 
-	$con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::MYSQL_ATTR_LOCAL_INFILE => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$handle = fopen($csvfile, 'r');
 
-        echo "Connected on. \n";
+if (!$handle) {
 
-}
-catch (PDOException $ex) {
-
-        die("DB connection failed: ".$e->getMessage());
+	die("Could not open the data file.\n");
 
 }
 
-$loaded = $con -> exec("LOAD DATA LOCAL INFILE ".$con->quote($csvfile)." INTO TABLE `$dbtable` FIELDS TERMINATED BY ".$con->quote($fieldseparator)." LINES TERMINATED BY ".$con->quote($lineseparator)." IGNORE 1 LINES (name, surname, email)");
+// to remove the first line
+fgetcsv($handle);
 
-echo "Loaded a total of $loaded records. \n";
+while (($data = fgetcsv($handle)) !== FALSE ) {
+		
+
+        $charlist = " \t\n\r\0..\x40\x5B..\x60\x7B..\x7F";
+
+
+	$name = ucwords(strtolower(trim($data[0], $charlist)));
+
+	$surname = ucwords(strtolower(trim($data[1], $charlist)));
+        
+        $surname = preg_replace_callback("/^O\'([a-z])/", function($match) { return strtoupper("$match[0]"); }, $surname);
+
+	$email = strtolower(ltrim(rtrim($data[2], $charlist)));
+
+        echo $name.", ".$surname.", ".$email,"\n";
+	
+	
+
+
+}
+
+fclose($handle);
+
+// try {
+
+// 	$con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::MYSQL_ATTR_LOCAL_INFILE => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+     
+
+// }
+// catch (PDOException $e) {
+
+//         die("DB connection failed: ".$e->getMessage());
+
+// }
+
+// echo "Server have been connected. \n";
+
+// $loaded = $con -> exec("LOAD DATA LOCAL INFILE ".$con->quote($csvfile)." INTO TABLE `$dbtable` FIELDS TERMINATED BY ".$con->quote($fieldseparator)." LINES TERMINATED BY ".$con->quote($lineseparator)." IGNORE 1 LINES (name, surname, email)");
+
+// echo "Loaded a total of $loaded records. \n";
 
 ?>
