f6aaa10ea3f0ce6d93ac19c1bd577328ee6356f3
katsla <katsla@yandex.ru>
2017-08-08 06:26:14 +1200

    refactoring, final version

--- user_upload.php
+++ user_upload.php
@@ -4,8 +4,7 @@
 
 require 'config.php';
 
-$con = null;
-$dry_run = false;
+$connection = [ "dry_run" => false, "connect" => $db ];
 $new_table = false;
 
 /* -------------------------
@@ -13,19 +12,37 @@ Set opts
 ----------------------------*/
 
 $help_message = "
-some help here
+Usage: user_upload.php [--help] [--create_table] [--dry_run] [--file=file] [-h] [-u] [-p]
+
+Process and insert (optional) values from an CSV file to a database.
+Optional: create table 'users' in the predefined database.
+Check initial settings in config.php.
+
+Options:
+
+        --help            Show this message
+        --create_table    Create table in DB and exit
+        --dry_run         Run script without altering of DB
+        --file            CSV file (default: users.csv)
+        -h                Server hostname (default: localhost)
+        -u                User (default: root)
+        -p                Password (default: root)
+
+Example:
+
+        user_upload.php --file=data.csv -u user -p psswrd 
+
 ";
 
 $shortopts = "h:p:u:";
 $longopts = array(
     "file:",
-    "create_table::",
+    "create_table",
     "dry_run",
     "help",
 );
 
 $opts = getopt($shortopts, $longopts);
-var_dump($opts);
 
 foreach (array_keys($opts) as $opt) switch ($opt) {
 
@@ -37,34 +54,32 @@ foreach (array_keys($opts) as $opt) switch ($opt) {
     case 'file':
         $csvfile = $opts['file'];
     case 'dry_run':
-        $dry_run = true;
+        $connection = [ "dry_run" => true, "connect" => [] ];
         break;
     case 'h':
-        $dbhost = $opts['h'];
-    case 'p':
-        $dbpsswd = $opts['p'];
+        $db['host'] = $opts['h'];
+    case "p":
+        $db['psswd'] = $opts["p"] ?? $db['psswd'];
     case 'u':
-        $dbuser = $opts['u'];
+        $db['user'] = $opts['u'] ?? $db['user'];
 }
 
 if ( empty($opts) ) { echo "Processing the script with default settings. Check 'config.php'.\n\n"; }
 
+/* -------------------------
+Start script
+----------------------------*/
+
 if ($new_table) {
 
-    connect_db($dbhost, $dbname, $dbuser, $dbpsswd);
-    create_table();
+    create_table($db['host'], $db['name'], $db['user'], $db['psswd']);
     $con = null;
-    exit("Table was created successfully.\n");
+    exit("Table was created/rebuilt successfully.\n");
 }
 
 file_exists($csvfile) or die("CSV file not found.\n");
 
-if (!$dry_run) { 
-
-    connect_db($dbhost, $dbname, $dbuser, $dbpsswd);
-}
-
-process_csv($csvfile, $dry_run);
+process_csv_and_insert($csvfile, $connection);
 
 $con = null;
 
@@ -77,8 +92,6 @@ Functions
 
 function connect_db($dbhost, $dbname, $dbuser, $dbpsswd) {
 
-    global $con;
-
     try {
         $con = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpsswd, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
@@ -87,20 +100,22 @@ function connect_db($dbhost, $dbname, $dbuser, $dbpsswd) {
     }
 
     echo "\nConnected to server.\n\n";
+
+    return $con;
 }
 
-function create_table() {
+function create_table($dbhost, $dbname, $dbuser, $dbpsswd) {
 
-    global $con;
+    $con = connect_db($dbhost, $dbname, $dbuser, $dbpsswd);
 
     $check = $con->prepare("SHOW TABLES LIKE 'users'");
     $check->execute();
     $table = $check->fetch(PDO::FETCH_NUM);
 
-    if (check_exit($table)) { 
+    if (check_drop_table($table)) { 
 
         $con = null;
-        exit("Connection was closed.\n"); 
+        exit("\nTable has not been rebuild.\nConnection is closed.\n"); 
     }
 
     $query = "
@@ -118,7 +133,7 @@ function create_table() {
     }
 }
 
-function check_exit($table) {
+function check_drop_table($table) {
     if (!empty($table)) {
 
         echo "The table exists. Are you sure you want to drop it? (y/N): ";
@@ -127,29 +142,46 @@ function check_exit($table) {
         $line = strtolower($line);
 
         fclose($handle);
-        if ( $line == 'y' ) { return true;  }
+        if ( $line !== 'y' ) { return true;  }
     }
     return false;
 }
 
-function insert_value($first_name, $last_name, $email) {
+function insert_value($first_name, $last_name, $email, $row_num, $con) {
 
-    global $con;
+    try {
+        $ins = $con->prepare("INSERT INTO users (name, surname, email) VALUES (:name, :surname, :email);");
+        $ins->bindValue(':name', $first_name);
+        $ins->bindValue(':surname', $last_name);
+        $ins->bindValue(':email', $email);
+        $ins->execute();
 
-    $ins = $con->prepare("INSERT INTO users (name, surname, email) VALUES (:name, :surname, :email);");
-    $ins->bindValue(':name', $first_name);
-    $ins->bindValue(':surname', $last_name);
-    $ins->bindValue(':email', $email);
-    $ins->execute();
+        echo "Inserted row $row_num: $first_name, $last_name, $email.\n";
+    }
+    catch (PDOException $e) {
+
+        $code = $e->getCode();
+
+        if ($code == '42S02' || $code == 'HY000') { die("Error: ".$e->getMessage()."\n"); }; //Table doesn't exist or read-only
+
+        fwrite(STDOUT, "DB insert failed on row $row_num: ".$e->getMessage()."\n");
+    }
 }
 
-function process_csv($csvfile, $dry_run ) {
+function process_csv_and_insert($csvfile, $connection ) {
 
     ini_set('auto_detect_line_endings', true);
 
     $handle = fopen($csvfile, 'r') or die("Could not open the data file.\n");
 
     fgetcsv($handle) or die("Incorrect CSV file!\n");
+
+    if (!$connection["dry_run"]) { 
+        $con = connect_db($connection["connect"]["host"], 
+                          $connection["connect"]["name"], 
+                          $connection["connect"]["user"], 
+                          $connection["connect"]["psswd"]);
+    }
 
     $row_num = 1;
 
@@ -166,24 +198,13 @@ function process_csv($csvfile, $dry_run ) {
 
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-            fwrite(STDOUT, "\nInvalid row $row_num: incorrect email $email.\n\n");
+            fwrite(STDOUT, "\nInvalid row $row_num: incorrect email $email. Insertion is denied.\n\n");
             continue;
         }
 
-        if (!$dry_run) {
-    
-            try { 
-                insert_value($first_name, $last_name, $email);
-                echo "Inserted row $row_num: $first_name, $last_name, $email.\n";
-            }
-            catch (PDOException $e) {
+        if (!$connection["dry_run"]) {
 
-                $code = $e->getCode();
-
-                if ($code == '42S02' || $code == 'HY000') { die("Error: ".$e->getMessage()."\n"); }; //Table doesn't exist or read-only
-
-                fwrite(STDOUT, "DB insert failed on row $row_num: ".$e->getMessage()."\n");
-            }
+            insert_value($first_name, $last_name, $email, $row_num, $con);
         } else {
 
             echo "Valid row $row_num: $first_name, $last_name, $email.\n";
